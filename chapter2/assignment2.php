f6119e833e0586a791bfe6beb9f481031e6cab71
Melissa Guevara Hernandez <mguevarahernandez07@gmail.com>
2026-09-07 11:46:16 -0700

    Add files via upload

---
+++ chapter2/assignment2.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Melissa Guevara Hernandez - Googleplex</title>
+</head>
+
+<body>
+
+<header>
+    <h1>Googleplex</h1>
+</header>
+
+<p>
+The Googleplex is the corporate headquarters complex of Google and its parent company Alphabet Inc. It is located at:
+<br>
+1600 Amphitheatre Parkway
+<br>
+Mountain View, CA, United States
+</p>
+
+</body>
+</html>
